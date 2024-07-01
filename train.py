8077baa905d208893f11025f3bc0c9d8c63e123d
CRYZOT <cristian.sotom@userena.cl>
2024-06-30 21:22:44 -0400

    entrenamiento con hiperparametros con yolon

--- train.py
+++ train.py
@@ -7,18 +7,18 @@ with open('config.yaml', 'r') as file:
 
 # Construir el comando de entrenamiento con los parámetros cargados
 command = (
-    f"yolo task=segment mode=train "
+    f"yolo task=detect mode=train "
     f"epochs={config['epochs']} "
-    f"data=Data/Cow/data.yaml "
-    f"model=yolov8m-seg.pt "
+    f"data=data.yaml "
+    f"model=yolov8n.pt "
     f"imgsz={config['imgsz']} "
     f"batch={config['batch']} "
     f"lr0={config['lr0']} "
     f"weight_decay={config['weight_decay']} "
     f"momentum={config['momentum']} "
-    f"patience={config['patience']} "  # Early stopping
-    f"cos_lr={config['cos_lr']} "      # Cosine Annealing LR
-    f"augment=True"                    # Aumentación de datos
+    f"patience={config['patience']} "   # Early stopping
+    f"cos_lr={config['cos_lr']} "       # Cosine Annealing LR
+    f"augment=True "                    # Aumentación de datos
 )
 
 print("Ejecutando comando:", command)
